47aa8bea98033348f82418e5e4a3ade4b420a3ef
tdanh16028 <tdanh16028@cusc.ctu.edu.vn>
2018-07-13 23:49:00 +0700

    Add data annotation to AdminAccount

--- ARSWebAPI/Models/AdminAccount.cs
+++ ARSWebAPI/Models/AdminAccount.cs
@@ -11,14 +11,28 @@ namespace ARSWebAPI.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
     public partial class AdminAccount
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
+
+        [Required]
+        [Index(IsUnique = true)]
         public string Username { get; set; }
+
+        [Required]
         public string Password { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Role { get; set; }
+
+        [Required]
         public bool IsActive { get; set; }
     }
 }
