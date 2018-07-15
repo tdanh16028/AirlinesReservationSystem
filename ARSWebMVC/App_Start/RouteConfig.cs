dc8a9b9bab9e6076b85d9bbd307cf9784d3e9552
tdanh16028 <tdanh16028@cusc.ctu.edu.vn>
2018-07-15 21:08:18 +0700

    Add function find all possible route between two city

--- ARSWebMVC/App_Start/RouteConfig.cs
+++ ARSWebMVC/App_Start/RouteConfig.cs
@@ -18,6 +18,7 @@ namespace ARSWebMVC
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
